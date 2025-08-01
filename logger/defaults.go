eed0584ace13f7d514adf3d3c19f5d9f01f41b92
W <w@abreu.app>
2025-07-31 20:51:59 -0400

    Implement EventWrapper to bridge with zerolog - Phase 1.2 complete

--- logger/defaults.go
+++ logger/defaults.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"time"
 
 	"github.com/rs/zerolog"
 )
@@ -15,6 +16,62 @@ type DefaultLogger struct {
 var Default = &DefaultLogger{}
 
 var logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Logger()
+
+// EventWrapper wraps a zerolog.Event to implement our Event interface
+type EventWrapper struct {
+	event *zerolog.Event
+}
+
+// Str implements the Event interface for EventWrapper
+func (e *EventWrapper) Str(key, val string) Event {
+	return &EventWrapper{event: e.event.Str(key, val)}
+}
+
+// Int implements the Event interface for EventWrapper
+func (e *EventWrapper) Int(key string, i int) Event {
+	return &EventWrapper{event: e.event.Int(key, i)}
+}
+
+// Int64 implements the Event interface for EventWrapper
+func (e *EventWrapper) Int64(key string, i int64) Event {
+	return &EventWrapper{event: e.event.Int64(key, i)}
+}
+
+// Float64 implements the Event interface for EventWrapper
+func (e *EventWrapper) Float64(key string, f float64) Event {
+	return &EventWrapper{event: e.event.Float64(key, f)}
+}
+
+// Bool implements the Event interface for EventWrapper
+func (e *EventWrapper) Bool(key string, b bool) Event {
+	return &EventWrapper{event: e.event.Bool(key, b)}
+}
+
+// Time implements the Event interface for EventWrapper
+func (e *EventWrapper) Time(key string, t time.Time) Event {
+	return &EventWrapper{event: e.event.Time(key, t)}
+}
+
+// Dur implements the Event interface for EventWrapper
+func (e *EventWrapper) Dur(key string, d time.Duration) Event {
+	return &EventWrapper{event: e.event.Dur(key, d)}
+}
+
+// Err implements the Event interface for EventWrapper
+func (e *EventWrapper) Err(err error) Event {
+	return &EventWrapper{event: e.event.Err(err)}
+}
+
+// Msg implements the Event interface for EventWrapper
+func (e *EventWrapper) Msg(msg string) {
+	e.event.Msg(safeLogf("%s", msg))
+}
+
+// Msgf implements the Event interface for EventWrapper
+func (e *EventWrapper) Msgf(format string, v ...interface{}) {
+	logString := fmt.Sprintf(format, v...)
+	e.event.Msg(safeLogf("%s", logString))
+}
 
 func cleanString(text string) string {
 	urlRegex := `[a-zA-Z][a-zA-Z0-9+.-]*:\/\/[a-zA-Z0-9+%/.\-:_?&=#@+]+`
@@ -84,4 +141,35 @@ func (*DefaultLogger) Fatal(format string) {
 func (*DefaultLogger) Fatalf(format string, v ...any) {
 	logString := fmt.Sprintf(format, v...)
 	logger.Fatal().Msg(safeLogf("%s", logString))
+}
+
+// InfoEvent implements the Logger interface for DefaultLogger
+func (*DefaultLogger) InfoEvent() Event {
+	return &EventWrapper{event: logger.Info()}
+}
+
+// DebugEvent implements the Logger interface for DefaultLogger
+func (*DefaultLogger) DebugEvent() Event {
+	debug := os.Getenv("DEBUG") == "true"
+	if debug {
+		return &EventWrapper{event: logger.Debug()}
+	}
+	// Return a no-op event when debug is disabled
+	nopLogger := zerolog.Nop()
+	return &EventWrapper{event: nopLogger.Debug()}
+}
+
+// WarnEvent implements the Logger interface for DefaultLogger
+func (*DefaultLogger) WarnEvent() Event {
+	return &EventWrapper{event: logger.Warn()}
+}
+
+// ErrorEvent implements the Logger interface for DefaultLogger
+func (*DefaultLogger) ErrorEvent() Event {
+	return &EventWrapper{event: logger.Error()}
+}
+
+// FatalEvent implements the Logger interface for DefaultLogger
+func (*DefaultLogger) FatalEvent() Event {
+	return &EventWrapper{event: logger.Fatal()}
 }
--- logger/interfaces.go
+++ logger/interfaces.go
@@ -1,5 +1,7 @@
 package logger
 
+import "time"
+
 type Logger interface {
 	Log(format string)
 	Logf(format string, v ...any)
@@ -15,4 +17,26 @@ type Logger interface {
 
 	Fatal(format string)
 	Fatalf(format string, v ...any)
+
+	// Structured logging methods
+	WithFields(fields map[string]interface{}) Logger
+	WithSensitiveField(key, value string) Logger
+	InfoEvent() Event
+	DebugEvent() Event
+	WarnEvent() Event
+	ErrorEvent() Event
+	FatalEvent() Event
+}
+
+type Event interface {
+	Str(key, val string) Event
+	Int(key string, i int) Event
+	Int64(key string, i int64) Event
+	Float64(key string, f float64) Event
+	Bool(key string, b bool) Event
+	Time(key string, t time.Time) Event
+	Dur(key string, d time.Duration) Event
+	Err(err error) Event
+	Msg(msg string)
+	Msgf(format string, v ...interface{})
 }
